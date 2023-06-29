2958a629d68f37292fb2972c3c4cfefd4bf4fbc5
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-06-29 15:30:21 +0200

    Confirmation desgin started
    Added first and last name

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -1,32 +1,50 @@
 import React, {useEffect, useState} from "react";
 import { NextPage } from "next";
-import { DriverInformation } from "@/utils/logic";
 
 
 const confirmationPage:NextPage = () => {
-    const [driverInfo, setDriverInfo] = useState<DriverInformation>();
+  
+  /* TODO: This is placeholder data, get the data from the server */
+  const data = {
+    firstName: "John",
+    lastName: "Doe",
+    homeAddress: "Landgreven 3, 4. sal, 1301 København K",
+    socialSecurityNumber: "42694325748",
+    drivingLicenseNumber: "234232434",
+    phoneNumber: "45+ 70 77 88 88",
+    email: "JohnDoe@gmail.com",
 
-    useEffect(() => {
-      const driverInfoString = sessionStorage.getItem('driverInfo');
-
-      if (driverInfoString) {
-        setDriverInfo(JSON.parse(driverInfoString))
-      }
-      else {console.error("driverInfo not found in sessionStorage")}
-
-      /* I will do this for all my keys */
-
-
-    }, []);
-
+    location: {address: "Niels Brocks Gade 11574 København", position:{lat: "55.672685", lng: "12.573920"}},
+    greenCarNumberPlate: "DC 11 534",
+    time: "12:45",
+    date: "02/04/2023",
+    speed: "50",
+    crashDescription: "I drove and then i hit a wall",
+    damageDescription: "Car is missing roof",
+  }
 
     return (
-        <div>
-            <div id="driverInformation">
-                <p>{driverInfo ? driverInfo.firstName : "First name"}</p>
-                <p>{driverInfo ? driverInfo.lastName : "First name"}</p>
+      <div className="flex flex-col">
+
+        {/* Driver information collected */}
+        <div className="rounded-lg bg-MainGreen-100 w-full p-2">
+          <div className="flex flex-row">
+            {/* First name */}
+            <div>
+              <p className="text-xs">First name:</p>
+              <p>{data.firstName}</p>
             </div>
+
+            {/* Last name */}
+            <div>
+              <p className="text-xs">Last name:</p>
+              <p>{data.lastName}</p>
+            </div>
+          </div>
+
+
         </div>
+      </div>
     )
 };
 
