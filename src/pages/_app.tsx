96503d1387c90c95ef57dc8996f3f6539ee8436d
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-08-09 01:26:57 +0200

    Fixed styling
    Fixed damage report style carrying over to admin and index pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,30 @@
 import { Navbar } from "@/components/navigation";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Navbar />
-      <div className="p-4">
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+
+  const { pathname } = useRouter();
+  
+  if (pathname.startsWith('/damagereport')) {
+    return (
+      <>
+        <Navbar />
+        <div className="p-4">
+          <Component {...pageProps} />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default MyApp;
