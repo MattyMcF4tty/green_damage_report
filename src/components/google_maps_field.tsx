22e35c1a54fea174af2c43c53cd1d458a7bba2a3
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-08-27 21:04:39 +0200

    Google markers now working

--- src/components/google_maps_field.tsx
+++ src/components/google_maps_field.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import React, { useEffect, useState } from "react";
 import Loading from "./loading";
 import { GetUserPosition, reportDataType } from "@/utils/utils";
@@ -18,12 +18,12 @@ const markerSize = {x: 30, z: 50}
 
 
 interface GoogleMapsFieldProps {
-startPos?: {lat: number | null, lng: number | null};
+  startPos: {lat: number, lng: number};
   startZoom?: number;
   showMap: boolean;
 
-    accidentLocation: {lat: number, lng: number};
-    setAccidentLocation: (newAccidentLocation: {lat: number, lng: number}) => void;
+  accidentLocation: {lat: number | null, lng: number | null};
+  setAccidentLocation: (newAccidentLocation: {lat: number, lng: number}) => void;
 
   bikes: bikeInformation[];
   setBikes: (newBikes: bikeInformation[]) => void;
@@ -39,44 +39,57 @@ startPos?: {lat: number | null, lng: number | null};
 }
 
 const GoogleMapsField = ({showMap, startZoom, startPos, bikes, setBikes, vehicles, setVehicles, pedestrians, setPedestrians, objects, setObjects, accidentLocation, setAccidentLocation}: GoogleMapsFieldProps) => {
-  const [mapPos, setMapPos] = useState(startPos || {lat: 0, lng: 0});
+  const [mapPos, setMapPos] = useState(startPos);
   const mapStartZoom = startZoom || 0;
-
+  /* Load Google maps javascript api */
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || ""
   })
+
+  /* Load previos location or where user is now */
+  useEffect(() => {
+    if (accidentLocation.lat !== null && accidentLocation.lng !== null) {
+      setMapPos({lat: accidentLocation.lat, lng: accidentLocation.lng});
+    } else {  
+      GetUserLocation()
+    }
+  }, [])
+  const GetUserLocation = async () => {
+      const userLocation = await GetUserPosition();
+      if (userLocation) {
+        setMapPos(userLocation);
+      }
+  };
 
   useEffect(() => {
     setAccidentLocation(mapPos)
   }, [mapPos])
 
-  const GetUserLocation = async () => {
-    const userLocation = await GetUserPosition();
-    if (userLocation) {
-      setMapPos(userLocation);
-    }
-  };
-
   const handleDragEnd = (index:number, position: {lat: number, lng: number}, type: "AccidentLocation" | "Bike" | "Vehicle" | "Pedestrian" | "Object") => {
     switch (type) {
         case "AccidentLocation":
-            setAccidentLocation(position);
+          setAccidentLocation(position);
+          break;
         case "Bike":
-            const updatedBikes = bikes;
-            updatedBikes[index].location = position;
-            setBikes(updatedBikes);
+          const updatedBikes = bikes;
+          updatedBikes[index].location = position;
+          setBikes(updatedBikes);
+          break;
         case "Vehicle":
-            const updatedVehicle = vehicles;
-            updatedVehicle[index].location = position;
-            setVehicles(updatedVehicle);
+          const updatedVehicle = vehicles;
+          updatedVehicle[index].location = position;
+          setVehicles(updatedVehicle);
+          break;
         case "Pedestrian":
-            const updatedPedestrian = pedestrians;
-            updatedPedestrian[index].location = position;
-            setPedestrians(updatedPedestrian);
+          const updatedPedestrian = pedestrians;
+          updatedPedestrian[index].location = position;
+          setPedestrians(updatedPedestrian);
+          break;
         case "Object": 
-            const updatedObjects = objects;
-            updatedObjects[index].location = position;
-            setObjects(updatedObjects);
+          const updatedObjects = objects;
+          updatedObjects[index].location = position;
+          setObjects(updatedObjects);
+          break;
     }
   }
 
@@ -91,30 +104,36 @@ const GoogleMapsField = ({showMap, startZoom, startPos, bikes, setBikes, vehicle
           ) : (isLoaded) ? (
             <div>
               <GoogleMap
-                onLoad={() => GetUserLocation()}
                 mapContainerClassName="w-full h-[400px] border-[1px] border-MainGreen-200 rounded-lg"
-                zoom={mapStartZoom}
+                zoom={startZoom}
                 center={mapPos}
+                options={{
+                  fullscreenControl: false,
+                  streetViewControl: false,
+                }} 
               >
-                <Marker
-                  position={mapPos} // Replace with the desired lat/lng coordinates
+                <MarkerF
+                  position={(mapPos)}
                   draggable={true}
                   icon={{
                     url: GreenCarIcon.src,
-                    scaledSize: new window.google.maps.Size(markerSize.x, markerSize.z),
+                    scaledSize: new window.google.maps.Size(50, 60),
                   }}
                   onDragEnd={(e) => {
                     if (e.latLng?.lat() && e.latLng?.lng()) {
-                        setAccidentLocation({lat: e.latLng.lat(), lng: e.latLng.lng()})
+                      setMapPos({lat: e.latLng.lat(), lng: e.latLng.lng()})
                     }
-                }}
+                  }}
                 />
-
                 {/* Spawns the amount of bikes thats given by user */}
                 {bikes.map((currentBike, index) => (
-                  <Marker
+                  <MarkerF
                     key={index}
-                    position={currentBike.location} 
+                    position={
+                      currentBike.location.lat !== null && currentBike.location.lng !== null
+                      ? {lat: currentBike.location.lat, lng: currentBike.location.lng}
+                      : { lat: mapPos.lat, lng: mapPos.lng - 0.0005 }
+                    } 
                     draggable={true}
 
                     icon={{
@@ -131,9 +150,13 @@ const GoogleMapsField = ({showMap, startZoom, startPos, bikes, setBikes, vehicle
 
                 {/* Spawns the amount of cars thats given by user */}
                 {vehicles.map((currentCar, index) => (
-                  <Marker
+                  <MarkerF
                     key={index}
-                    position={{lat: mapPos.lat+0.0005, lng: mapPos.lng+0.0005}} 
+                    position={
+                      currentCar.location.lat !== null && currentCar.location.lng !== null
+                      ? {lat: currentCar.location.lat, lng: currentCar.location.lng}
+                      : { lat: mapPos.lat, lng: mapPos.lng + 0.0005 }
+                    }                     
                     draggable={true}
                     icon={{
                         url: CarIcon.src,
@@ -149,9 +172,13 @@ const GoogleMapsField = ({showMap, startZoom, startPos, bikes, setBikes, vehicle
 
                 {/* Spawns the amount of pedestrians thats given by user */}
                 {pedestrians.map((currentPedestrians, index) => (
-                  <Marker
+                  <MarkerF
                     key={index}
-                    position={{lat: mapPos.lat-0.0005, lng: mapPos.lng-0.0005}} 
+                    position={
+                      currentPedestrians.location.lat !== null && currentPedestrians.location.lng !== null
+                      ? {lat: currentPedestrians.location.lat, lng: currentPedestrians.location.lng}
+                      : { lat: mapPos.lat - 0.00025, lng: mapPos.lng}
+                    } 
                     draggable={true}
                     icon={{
                         url: PersonIcon.src,
@@ -167,9 +194,13 @@ const GoogleMapsField = ({showMap, startZoom, startPos, bikes, setBikes, vehicle
 
                 {/* Spawns the amount of objects thats given by user */}
                 {objects.map((currentObjects, index) => (
-                  <Marker
+                  <MarkerF
                     key={index}
-                    position={{lat: mapPos.lat-0.0005, lng: mapPos.lng-0.0005}} 
+                    position={
+                      currentObjects.location.lat !== null && currentObjects.location.lng !== null
+                      ? {lat: currentObjects.location.lat, lng: currentObjects.location.lng}
+                      : { lat: mapPos.lat + 0.00025, lng: mapPos.lng}
+                    } 
                     draggable={true}
                     icon={{
                         url: ObjectIcon.src,
