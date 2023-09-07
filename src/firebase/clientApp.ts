bb918d60802fda0daabd9a83061b7f325ec8e371
Carloslund <carloslundrodriguez@gmail.com>
2023-09-07 14:57:48 +0200

    downloading google maps successfully

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,9 +1,33 @@
-import { initializeApp } from "firebase/app"
-import { collection, deleteDoc, doc, getDoc, getDocs, getFirestore, query, setDoc, updateDoc, where } from "firebase/firestore";
-import { ListResult, StorageReference, deleteObject, getDownloadURL, getStorage, listAll, ref, uploadBytes } from "firebase/storage"
-import { decryptData, downloadToPc, encryptData, reportDataType } from "@/utils/utils";
+import { initializeApp } from "firebase/app";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import {
+  ListResult,
+  StorageReference,
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  listAll,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import {
+  decryptData,
+  downloadToPc,
+  encryptData,
+  reportDataType,
+} from "@/utils/utils";
 import axios from "axios";
-
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -19,56 +43,58 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-const collectionName = "DamageReports"
+const collectionName = "DamageReports";
 
 export const getData = async (id: string) => {
-    console.log("fetchind docID: " + id)
-    const docRef = doc(db, `${collectionName}/${id}`);
-    const data = new reportDataType()
+  console.log("fetchind docID: " + id);
+  const docRef = doc(db, `${collectionName}/${id}`);
+  const data = new reportDataType();
 
-    try {
-        const docSnapshot = await getDoc(docRef)
-        if (docSnapshot.exists()) {
-            const fetchedData = docSnapshot.data();
-            data.updateFields(fetchedData)
-        } 
-        else {
-            throw new Error("Document does not exist");
-        }
-    } catch (error) {
-        console.error(`Something went wrong fetching data:\n${error}\n`)
+  try {
+    const docSnapshot = await getDoc(docRef);
+    if (docSnapshot.exists()) {
+      const fetchedData = docSnapshot.data();
+      data.updateFields(fetchedData);
+    } else {
+      throw new Error("Document does not exist");
     }
-    
-    const decryptedData = decryptData(data)
-    return decryptedData;
-}
+  } catch (error) {
+    console.error(`Something went wrong fetching data:\n${error}\n`);
+  }
+
+  const decryptedData = decryptData(data);
+  return decryptedData;
+};
 
 export const createDoc = async (id: string, email: string) => {
-    try {
-        const data = new reportDataType()
-        data.updateFields({userEmail: email.toLowerCase()})
-        console.log("Report created:\n" + "id: " + id + "\n" + "Email: " + email.toLowerCase());
+  try {
+    const data = new reportDataType();
+    data.updateFields({ userEmail: email.toLowerCase() });
+    console.log(
+      "Report created:\n" + "id: " + id + "\n" + "Email: " + email.toLowerCase()
+    );
 
-        const dataRef = doc(db, `${collectionName}/${id}`);
-        await setDoc(dataRef, data.toPlainObject());
-    } catch (error) {
-        console.error(`An error occurred while writing data:\n${error}`);
-    }
-}
-
-export const updateData = async (id:string, data:reportDataType) => {
     const dataRef = doc(db, `${collectionName}/${id}`);
-    const currentDate = new Date();
+    await setDoc(dataRef, data.toPlainObject());
+  } catch (error) {
+    console.error(`An error occurred while writing data:\n${error}`);
+  }
+};
 
+export const updateData = async (id: string, data: reportDataType) => {
+  const dataRef = doc(db, `${collectionName}/${id}`);
+  const currentDate = new Date();
 
-    try {
-        const encryptedData = encryptData(data);
-        await updateDoc(dataRef, encryptedData.toPlainObject());
-        await updateDoc(dataRef, {lastChange: `${currentDate.getHours()}:${currentDate.getMinutes()} - ${currentDate.getDay()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`})
-    } catch (error) {
-        console.error(`Something went wrong updating data:\n${error}`)
-    }
-}
+  try {
+    const encryptedData = encryptData(data);
+    await updateDoc(dataRef, encryptedData.toPlainObject());
+    await updateDoc(dataRef, {
+      lastChange: `${currentDate.getHours()}:${currentDate.getMinutes()} - ${currentDate.getDay()}/${currentDate.getMonth()}/${currentDate.getFullYear()}`,
+    });
+  } catch (error) {
+    console.error(`Something went wrong updating data:\n${error}`);
+  }
+};
 
 export const updateImages = async (
   id: string,
@@ -101,16 +127,19 @@ const deleteAllImages = async (StorageRef: StorageReference) => {
   try {
     const storedImages: ListResult = await listAll(StorageRef);
 
-        const deletedImages:Promise<void>[] = storedImages.items.map(async (image) => {
-            await deleteObject(image);
-        })
+    const deletedImages: Promise<void>[] = storedImages.items.map(
+      async (image) => {
+        await deleteObject(image);
+      }
+    );
 
-        await Promise.all(deletedImages)
-    }
-    catch ( error ) {
-        console.error(`Something went wrong deleting all images in ${StorageRef.name}:\n${error}\n`)
-    }
-}
+    await Promise.all(deletedImages);
+  } catch (error) {
+    console.error(
+      `Something went wrong deleting all images in ${StorageRef.name}:\n${error}\n`
+    );
+  }
+};
 
 export const getImages = async (id: string) => {
   const greenStorageRef = ref(storage, `${id}/GreenMobility`);
@@ -120,9 +149,9 @@ export const getImages = async (id: string) => {
     OtherParty: [],
   };
 
-    try {
-        const greenImageList: ListResult = await listAll(greenStorageRef);
-        const otherPartyImageList: ListResult = await listAll(otherPartyStorageRef);
+  try {
+    const greenImageList: ListResult = await listAll(greenStorageRef);
+    const otherPartyImageList: ListResult = await listAll(otherPartyStorageRef);
 
     /* Get images of GreenMobility car */
     const greenImageURLs: string[] = [];
@@ -172,23 +201,23 @@ export const getReportIds = async () => {
 };
 
 export const getReports = async () => {
-    const idList = await getReportIds();
-    const reportList: { id: string; data: reportDataType }[] = [];
+  const idList = await getReportIds();
+  const reportList: { id: string; data: reportDataType }[] = [];
 
-    if (idList.length > 0) {  
-        try {
-            await Promise.all(
-                idList.map(async (id) => {
-                  const docData = await getData(id);
-                  if (docData !== undefined) {
-                      reportList.push({ id: id, data: docData });
-                  }
-                })
-            );
-        } catch(error) {
-            console.error(`Something went wrong fecthing reports:\n${error}\n`)
-        }
+  if (idList.length > 0) {
+    try {
+      await Promise.all(
+        idList.map(async (id) => {
+          const docData = await getData(id);
+          if (docData !== undefined) {
+            reportList.push({ id: id, data: docData });
+          }
+        })
+      );
+    } catch (error) {
+      console.error(`Something went wrong fecthing reports:\n${error}\n`);
     }
+  }
 
   return reportList;
 };
@@ -202,69 +231,78 @@ export const deleteReports = async (reportsToDelete: string[]) => {
         throw Error("No reports to delete");
       }
 
-            for (const report of reportsToDelete) {
-                const reportRef = doc(db, `${collectionName}/${report}`);
-                await deleteDoc(reportRef);
-            }
-        } catch (error) {
-            console.error(`Error deleting ${reportRef.path}: ${error}`);
-        }
-    })
-}
-
-export const checkEmailExists = async (email: string) => {
-    const collectionRef = collection(db, collectionName)
-    const q = query(collectionRef, where("userEmail", "==", email.toLowerCase()));
-    
-    try {
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(doc => (console.log(doc.id)))
-
-      if (querySnapshot) {
-        const reportIDs = querySnapshot.docs.map((doc) => doc.id)
-        return reportIDs;
-      } else {
-
-        return [];
+      for (const report of reportsToDelete) {
+        const reportRef = doc(db, `${collectionName}/${report}`);
+        await deleteDoc(reportRef);
       }
     } catch (error) {
-      console.error(`An error occurred while checking email:\n${error}`);
+      console.error(`Error deleting ${reportRef.path}: ${error}`);
+    }
+  });
+};
+
+export const checkEmailExists = async (email: string) => {
+  const collectionRef = collection(db, collectionName);
+  const q = query(collectionRef, where("userEmail", "==", email.toLowerCase()));
+
+  try {
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => console.log(doc.id));
+
+    if (querySnapshot) {
+      const reportIDs = querySnapshot.docs.map((doc) => doc.id);
+      return reportIDs;
+    } else {
       return [];
     }
-}
+  } catch (error) {
+    console.error(`An error occurred while checking email:\n${error}`);
+    return [];
+  }
+};
 
-export const handleGeneratePdf = async (id: string)  => {
+export const handleGeneratePdf = async (id: string) => {
   try {
-    const response = await axios.post('/api/generatepdf', { id });
+    const response = await axios.post("/api/generatepdf", { id });
     if (response.status === 200) {
       console.log(`PDF of ${id} generated and uploaded successfully.`);
     }
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error("An error occurred:", error);
   }
-}
+};
 
 export const handleDownloadPdf = async (id: string) => {
   try {
-    const response = await axios.post('/api/downloadpdf', { id }, { responseType: 'arraybuffer' });
+    const response = await axios.post(
+      "/api/downloadpdf",
+      { id },
+      { responseType: "arraybuffer" }
+    );
 
     if (response.status === 200) {
       console.log(`PDF of ${id} downloaded from server successfully.`);
-      
-      const pdfBlob: Blob = new Blob([response.data], { type: 'application/pdf' });
-      
+
+      const pdfBlob: Blob = new Blob([response.data], {
+        type: "application/pdf",
+      });
+
       await downloadToPc(pdfBlob, `DamageReport_${id}`);
     } else {
       throw new Error("Something went wrong downloading pdf to pc");
     }
-  } catch (error:any) {
+  } catch (error: any) {
     if (error.response && error.response.status === 404) {
-      console.log('PDF does not exist, creating a new one.');
+      console.log("PDF does not exist, creating a new one.");
       await handleGeneratePdf(id);
       await handleDownloadPdf(id);
     } else {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   }
-}
+};
+export const handleUploadMap = async (mapImg: Blob, id: string) => {
+  const storageRef = ref(storage, `${id}/admin/map`);
 
+  await uploadBytes(storageRef, mapImg);
+};
