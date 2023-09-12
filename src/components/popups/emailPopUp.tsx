39916d14204a520c3206a80e36e39696bcf401f7
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-09-12 11:36:48 +0200

    Small design changes

--- src/components/popups/emailPopUp.tsx
+++ src/components/popups/emailPopUp.tsx
@@ -22,7 +22,7 @@ const EmailPopUp = ({reportIDs, setVisibility, email}: EmailPopUpProps) => {
 
     return (
         <div className="fixed flex justify-center items-center z-20 inset-0 bg-black bg-opacity-75 overflow-auto">
-            <div className="bg-white w-[20rem] p-3">
+            <div className="bg-white w-[20rem] p-4 rounded-md">
                 <p>I looks like you already have ongoing reports, would you like to continue with one of them?</p>
                 <p className="mt-2">Reports:</p>
                 <div className="flex flex-col">
@@ -34,9 +34,9 @@ const EmailPopUp = ({reportIDs, setVisibility, email}: EmailPopUpProps) => {
                 </div>
                 <div className="flex flex-row h-[2.4rem] mt-10 justify-evenly">
                     <button disabled={!enableButtons} onClick={() => {setVisibility(false)}} 
-                    className="bg-gray-200 p-1 h-full w-24">Cancel</button>
+                    className="bg-gray-200 p-1 h-full w-24 rounded-sm">Cancel</button>
                     <button disabled={!enableButtons} onClick={() => {handleNewReport()}} 
-                    className="bg-MainGreen-300 p-1 h-full w-24 text-white">New report</button>
+                    className="bg-MainGreen-300 p-1 h-full w-24 text-white rounded-sm">New report</button>
                 </div>
             </div>            
         </div>
