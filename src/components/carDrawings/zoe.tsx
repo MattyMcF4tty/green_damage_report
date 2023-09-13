d710adde48ba080be6d52a2866458bd77a61455b
Carloslund <carloslundrodriguez@gmail.com>
2023-09-13 17:33:57 +0200

    made some more buttons

--- src/components/carDrawings/zoe.tsx
+++ src/components/carDrawings/zoe.tsx
@@ -3,35 +3,75 @@ const ZoeDrawing = () => {
     <div className="w-[22rem]">
       <button
         type="button"
-        className="absolute rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[17rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
-        w-5 h-5 lg:flex lg:items-center lg:pl-[0.4rem] top-[19.3rem] left-[10.5rem] hover:w-6 hover:h-6 hover:top-[19.1rem] hover:pl-[5px] duration-150 flex items-center pl-1 
-        "
-      >
-        2
-      </button>
-      <button
-        type="button"
-        className="absolute rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[11.7rem] lg:hover:ml-[11.56rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.5rem] lg:duration-150 
-        w-5 h-5 lg:flex lg:items-center lg:pl-[0.25rem] top-[19.9482rem] left-[7.5rem] hover:w-6 hover:h-6 hover:left-[7.4rem] hover:top-[19.84rem] hover:pl-[5px] duration-150 flex items-center pl-1 
+        className="absolute bg-MainGreen-300 text-white rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[11.7rem] lg:hover:ml-[11.56rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.5rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.25rem] text-xs top-[20.02rem] left-[7.56rem] hover:w-6 hover:h-6 hover:left-[7.4rem] hover:top-[19.84rem] hover:pl-[0.45rem] duration-150 flex items-center pl-1 
         "
       >
         1
       </button>
       <button
         type="button"
-        className="absolute rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[24.78rem] lg:hover:ml-[24.65rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.35rem] lg:duration-150 
-        w-5 h-5 lg:flex lg:items-center lg:pl-[0.25rem] mt-[3.14rem] ml-[14.115rem] hover:w-6 hover:h-6 hover:mt-[3rem] hover:ml-[14rem] hover:pl-[5px] duration-150 flex items-center pl-[0.15rem]
+        className="absolute bg-MainGreen-300 text-white rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[17rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.4rem] top-[19.3rem] left-[10.5rem] hover:w-6 hover:h-6 hover:top-[19.1rem] hover:pl-[0.314rem] duration-150 flex items-center pl-[0.13rem] 
+        "
+      >
+        2
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[21.5rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.4rem] top-[19.3rem] left-[12.8rem] hover:w-6 hover:h-6 hover:top-[19.1rem] hover:pl-[0.314rem] duration-150 flex items-center pl-[0.13rem] 
+        "
+      >
+        3
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[24.78rem] lg:hover:ml-[24.65rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.35rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.25rem] top-[20.02rem] left-[14.94rem] hover:w-6 hover:h-6 hover:top-[19.85rem] hover:left-[14.75rem] hover:pl-[0.29rem] duration-150 flex items-center pl-[0.08rem]
         "
       >
         4
       </button>
       <button
         type="button"
-        className="absolute rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[21.5rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
-        w-5 h-5 lg:flex lg:items-center lg:pl-[0.4rem] top-[19.3rem] left-[12.8rem] hover:w-6 hover:h-6 hover:top-[19.1rem] hover:pl-[5px] duration-150 flex items-center pl-[0.2rem] 
+        className="absolute bg-MainGreen-300 text-white rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[11.7rem] lg:hover:ml-[11.56rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.5rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.25rem] top-[23.3rem] left-[6.5rem] hover:w-6 hover:h-6 hover:top-[23.2rem] hover:pl-[0.35rem] duration-150 flex items-center pl-[0.12rem]
         "
       >
-        3
+        5
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[17rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.4rem] top-[23.3rem] left-[9.2rem] hover:w-6 hover:h-6 hover:top-[23.2rem] hover:pl-[5px] duration-150 flex items-center pl-[0.1rem] 
+        "
+      >
+        6
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-2 lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[4.3rem] lg:text-3xl lg:ml-[21.5rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[4.25rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.3rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.4rem] top-[23.3rem] left-[12.3rem] hover:w-6 hover:h-6 hover:top-[23.2rem] hover:pl-[5px] duration-150 flex items-center pl-[0.18rem] 
+        "
+      >
+        7
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[24.78rem] lg:hover:ml-[24.65rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.35rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.25rem] top-[23.3rem] left-[15.2rem] hover:w-6 hover:h-6 hover:top-[23.3rem] hover:pl-[0.3rem] duration-150 flex items-center pl-[0.11rem]
+        "
+      >
+        8
+      </button>
+      <button
+        type="button"
+        className="absolute bg-MainGreen-300 text-white rounded-full border-[2px] lg:h-8 lg:w-8 border-MainGreen-300 lg:mt-[5.55rem] lg:text-3xl lg:ml-[24.78rem] lg:hover:ml-[24.65rem] lg:hover:w-9 lg:hover:h-9 lg:hover:mt-[5.45rem] lg:hover:flex lg:hover:justify-center lg:hover:pr-[0.35rem] lg:duration-150 
+        w-[1.1rem] h-[1.1rem] lg:flex lg:items-center lg:pl-[0.25rem] top-[23.3rem] left-[16.9rem] hover:w-6 hover:h-6 hover:top-[23.3rem] hover:pl-[0.3rem] duration-150 flex items-center pl-[0.12rem]
+        "
+      >
+        9
       </button>
       <img src="../carDrawings/zoePlantegning.png" alt="ZoeDrawing" />
     </div>
