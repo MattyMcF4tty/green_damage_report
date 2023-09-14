196cce7f6c2a6aa38244edc67cf5a0fda14d3668
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-09-15 00:12:58 +0200

    Now possible to get renter

--- src/pages/api/wunderfleet/getRenter.ts
+++ src/pages/api/wunderfleet/getRenter.ts
@@ -1,3 +1,4 @@
+import { getAge } from "@/utils/utils";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
@@ -5,8 +6,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     try {
         const { numberplate } = req.body;
         const wunderUrl = process.env.WUNDER_STAGING_DOMAIN as string;
-        const accessToken = process.env.WUNDER_ACCESS_TOKEN as string;
-        const testNumberplate = "CX 86 711"
+        const accessToken = process.env.WUNDER_STAGING_TOKEN as string;
 
         if (!wunderUrl) {
             return res.status(400).json({ message: "Missing wunderfleet api url" })
@@ -14,32 +14,87 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         if (!accessToken) {
             return res.status(400).json({ message: "Missing wunderfleet access token" })
         }
-        if (!numberplate) {
+        if (numberplate === null) {
             return res.status(400).json({ message: "Missing numberplate" })
         }
 
-        const response = await fetch(wunderUrl + "/api/v2/vehicles/search", {
+        // Get information about vehicle
+        const vehicleResponse = await fetch(wunderUrl + "/api/v2/vehicles/search", {
             method: 'POST',
             headers: {
-                'accept': 'application/json',
-                'Authorization': accessToken,
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
             },
             body: JSON.stringify({
                 "licencePlate": {
-                    "$gte": testNumberplate
+                    "$eq": numberplate
                 }
             })
         })
-
-        const responseData = await response.json();
-        console.log(responseData.errors)
-        if (!response.ok) {
-            return res.status(500).json({ message: `Something went wrong contacting wunderfleet:`})
+        const vehicleResponseData = await vehicleResponse.json();
+        if (!vehicleResponse.ok) {
+            return res.status(500).json({ message: `Something went wrong fetching vehicle info by numberplate`, error: vehicleResponseData.errors})
         }
-        const renter = {davs: "sdgrg"}
+        const carId = vehicleResponseData.data[0].vehicleId
+
+        //Get reservation information from the cars id
+        const reservationResponse = await fetch(wunderUrl + "/api/v2/reservations/search", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+                "carId": {
+                    "$eq": carId
+                }
+            })
+        })
+        const reservationResponseData = await reservationResponse.json();
+        if (!vehicleResponse.ok) {
+            return res.status(500).json({ message: `Something went wrong fetching reservation info by carId`, error: reservationResponseData.errors})
+        }
+        const reservations = reservationResponseData.data.length-1 as number;
+        const newestReservation = reservationResponseData.data[reservations];
+        const reservationId =  newestReservation.reservationId;
+        const customerId = newestReservation.customerId;
+
+        const renterResponse = await fetch(wunderUrl + "/api/v2/customers/search", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }, 
+            body: JSON.stringify(
+                {
+                  "customerId": {
+                    "$eq": 1265
+                  }
+                })
+        })
+        const renterResponseData = await renterResponse.json();
+        if (!renterResponse.ok) {
+            return res.status(500).json({ message: `Something went wrong fetching reservation info by carId`, error: reservationResponseData.errors})
+        }
+
+        const renterData = renterResponseData.data[0];
+
+
+        const renterAge = getAge(renterData.birthDate)
+
+        const renterInfo = {
+            customerId: customerId as number, 
+            reservationId: reservationId as number,
+            firstName: `${renterData.firstName}`,
+            lastName: `${renterData.lastName}`,
+            birthDate: `${renterData.birthDate}`,
+            gender: `${renterData.gender}`,
+            age: renterAge,
+            insurance: null,
+        };
 
         
-        return res.status(200).json({ message: "Renter fecthing from wunderfleet completed succesfully", data: renter})
+        return res.status(200).json({ message: "Renter fecthing from wunderfleet completed succesfully", data: renterInfo})
     } catch (error) {
         return res.status(500).json({ message: `Something went wrong getting renter:\n${error}`})
     }
